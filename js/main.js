02fa2c6791ea53221d6ff56fe252710fff4b3e03
Brenden Callahan <brendenkcallahan@gmail.com>
2022-12-22 16:12:12 -0800

    V 0.75

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-//placeholder
+const hamburger = document.querySelector(".burger-icon");
+const navMenu = document.querySelector(".burger-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".burger-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
